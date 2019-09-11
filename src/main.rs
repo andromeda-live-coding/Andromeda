1f197dd813dbc4e3c3867d15bd72b47b45c8f25f
Federico Dolce <psykopear@gmail.com>
2019-09-11 11:03:38 +0200

    Trying to parse expressions

--- src/main.rs
+++ src/main.rs
@@ -1,216 +1,215 @@
 mod parser;
-use colored::Colorize;
-use nannou::app::Draw;
-use nannou::prelude::*;
-use nannou::ui::prelude::*;
-use parser::Command;
-use std::collections::HashMap;
+// use colored::Colorize;
+// use nannou::app::Draw;
+// use nannou::prelude::*;
+// use nannou::ui::prelude::*;
+// use std::collections::HashMap;
 fn main() {
-    let p = crate::parser::expr("+7-2*(9*8)");
-    println!("{:#?}", p);
-    nannou::app(model)
-        .event(event)
-        .update(update)
-        .view(view)
-        .run();
+    // let p = crate::parser::parser("+7-2*(9*8)");
+    // println!("{:#?}", p);
+    // nannou::app(model)
+    //     .event(event)
+    //     .update(update)
+    //     .view(view)
+    //     .run();
 }
 
-struct Model {
-    ui: Ui,
-    ids: Ids,
-    text_edit: String,
-    variables: HashMap<String, f32>,
-    instructions: Vec<Command>,
-}
+// struct Model {
+//     ui: Ui,
+//     ids: Ids,
+//     text_edit: String,
+//     variables: HashMap<String, f32>,
+//     instructions: Vec<Command>,
+// }
 
-struct Ids {
-    text_edit: widget::Id,
-}
+// struct Ids {
+//     text_edit: widget::Id,
+// }
 
-fn model(app: &App) -> Model {
-    app.set_loop_mode(LoopMode::wait(3));
-    app.new_window()
-        .with_dimensions(720, 720)
-        .event(window_event)
-        .raw_event(raw_window_event)
-        .key_pressed(key_pressed)
-        .key_released(key_released)
-        .mouse_moved(mouse_moved)
-        .mouse_pressed(mouse_pressed)
-        .mouse_released(mouse_released)
-        .mouse_wheel(mouse_wheel)
-        .mouse_entered(mouse_entered)
-        .mouse_exited(mouse_exited)
-        .moved(window_moved)
-        .resized(window_resized)
-        .focused(window_focused)
-        .unfocused(window_unfocused)
-        .closed(window_closed)
-        .build()
-        .unwrap();
-    // Create the UI.
-    let mut ui = app.new_ui().build().unwrap();
-    ui.fonts_mut()
-        .insert_from_file("F25_Bank_Printer.ttf")
-        .unwrap();
-    // Generate some ids for our widgets.
-    let ids = Ids {
-        text_edit: ui.generate_widget_id(),
-    };
+// fn model(app: &App) -> Model {
+//     app.set_loop_mode(LoopMode::wait(3));
+//     app.new_window()
+//         .with_dimensions(720, 720)
+//         .event(window_event)
+//         .raw_event(raw_window_event)
+//         .key_pressed(key_pressed)
+//         .key_released(key_released)
+//         .mouse_moved(mouse_moved)
+//         .mouse_pressed(mouse_pressed)
+//         .mouse_released(mouse_released)
+//         .mouse_wheel(mouse_wheel)
+//         .mouse_entered(mouse_entered)
+//         .mouse_exited(mouse_exited)
+//         .moved(window_moved)
+//         .resized(window_resized)
+//         .focused(window_focused)
+//         .unfocused(window_unfocused)
+//         .closed(window_closed)
+//         .build()
+//         .unwrap();
+//     // Create the UI.
+//     let mut ui = app.new_ui().build().unwrap();
+//     ui.fonts_mut()
+//         .insert_from_file("F25_Bank_Printer.ttf")
+//         .unwrap();
+//     // Generate some ids for our widgets.
+//     let ids = Ids {
+//         text_edit: ui.generate_widget_id(),
+//     };
 
-    // Init our variables
-    let text_edit = "".to_string();
-    let variables = HashMap::new();
-    let instructions: Vec<Command> = Vec::new();
+//     // Init our variables
+//     let text_edit = "".to_string();
+//     let variables = HashMap::new();
+//     let instructions: Vec<Command> = Vec::new();
 
-    Model {
-        ui,
-        ids,
-        text_edit,
-        variables,
-        instructions,
-    }
-}
+//     Model {
+//         ui,
+//         ids,
+//         text_edit,
+//         variables,
+//         instructions,
+//     }
+// }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    let ui = &mut model.ui.set_widgets();
+// fn update(_app: &App, model: &mut Model, _update: Update) {
+//     let ui = &mut model.ui.set_widgets();
 
-    if let Some(edit) = widget::TextEdit::new(&model.text_edit)
-        .color(color::WHITE)
-        .font_size(16)
-        .top_left_with_margin(10.0)
-        .line_spacing(2.5)
-        .restrict_to_height(false)
-        .set(model.ids.text_edit, ui)
-    {
-        model.text_edit = edit;
-    }
-}
+//     if let Some(edit) = widget::TextEdit::new(&model.text_edit)
+//         .color(color::WHITE)
+//         .font_size(16)
+//         .top_left_with_margin(10.0)
+//         .line_spacing(2.5)
+//         .restrict_to_height(false)
+//         .set(model.ids.text_edit, ui)
+//     {
+//         model.text_edit = edit;
+//     }
+// }
 
-fn view(app: &App, model: &Model, frame: &Frame) {
-    let draw = app.draw();
-    let position: (f32, f32) = (0.0, 0.0);
-    let std_value = 10.0;
-    draw.background().rgb(0.39, 0.39, 0.39);
-    let color = rgb(1.0, 1.0, 1.0);
-    for x in &model.instructions {
-        match x {
-            Command::VariableName(_) => {}
-            Command::DeclareVariable((_, _)) => {}
-            Command::DrawShape(shape) => {
-                draw.quad()
-                     .x_y(position.0, position.1)
-                     .w_h(std_value, std_value)
-                     .color(color);
-            }
-            Command::DrawShapeWf32((shape, val1, val2)) => {
-                draw.quad()
-                     .x_y(position.0, position.1)
-                     // eval Vec<Operation>
-                     .w_h(0.0, 0.0)
-                     .color(color);
-            }
-        }
-    }
-    // Write the result of our drawing to the window's frame.
-    draw.to_frame(app, &frame).unwrap();
+// fn view(app: &App, model: &Model, frame: &Frame) {
+//     let draw = app.draw();
+//     let position: (f32, f32) = (0.0, 0.0);
+//     let std_value = 10.0;
+//     draw.background().rgb(0.39, 0.39, 0.39);
+//     let color = rgb(1.0, 1.0, 1.0);
+//     for x in &model.instructions {
+//         match x {
+//             Command::VariableName(_) => {}
+//             Command::DeclareVariable((_, _)) => {}
+//             Command::DrawShape(shape) => {
+//                 draw.quad()
+//                     .x_y(position.0, position.1)
+//                     .w_h(std_value, std_value)
+//                     .color(color);
+//             }
+//             Command::DrawShapeWf32((shape, val1, val2)) => {
+//                 draw.quad()
+//                     .x_y(position.0, position.1)
+//                     // eval Vec<Operation>
+//                     .w_h(0.0, 0.0)
+//                     .color(color);
+//             }
+//         }
+//     }
+//     // Write the result of our drawing to the window's frame.
+//     draw.to_frame(app, &frame).unwrap();
 
-    // Draw the state of the `Ui` to the frame.
-    model.ui.draw_to_frame(app, &frame).unwrap();
-}
+//     // Draw the state of the `Ui` to the frame.
+//     model.ui.draw_to_frame(app, &frame).unwrap();
+// }
 
-fn window_event(_app: &App, model: &mut Model, event: WindowEvent) {
-    match event {
-        KeyPressed(key) => {
-            // when user press Lcontrol
-            if key == nannou::prelude::Key::LControl {
-                model.variables = HashMap::new();
-                if let Ok((remaining, ast)) = parser::parser(&model.text_edit) {
-                    let mut syntactic_analysis = true;
-                    if remaining != "" {
-                        syntactic_analysis = false;
-                    }
-                    // storing variables
-                    for x in ast.to_owned() {
-                        match x {
-                            Command::DeclareVariable((key, value)) => {
-                                // eval Vec<Operation>
-                                model.variables.insert(key, 0.0);
-                            }
-                            _ => (),
-                        }
-                    }
-                    // we update our model only if
-                    if syntactic_analysis {
-                        model.instructions = ast;
-                    } else {
-                        println!("{}", "CAN'T UPDATE ABSTRACT SYNTAX TREE".red().bold());
-                        println!("{:#?}", parser::parser(&model.text_edit));
-                        println!("{} {}", "error:".red().bold(), &remaining.red().bold());
-                    }
-                }
-            }
-        }
-        KeyReleased(_key) => {}
-        MouseMoved(_pos) => {}
-        MousePressed(_button) => {}
-        MouseReleased(_button) => {}
-        MouseEntered => {}
-        MouseExited => {}
-        MouseWheel(_amount, _phase) => {}
-        Moved(_pos) => {}
-        Resized(_size) => {}
-        Touch(_touch) => {}
-        TouchPressure(_pressure) => {}
-        HoveredFile(_path) => {}
-        DroppedFile(_path) => {}
-        HoveredFileCancelled => {}
-        Focused => {}
-        Unfocused => {}
-        Closed => {}
-    }
-}
+// fn window_event(_app: &App, model: &mut Model, event: WindowEvent) {
+//     match event {
+//         KeyPressed(key) => {
+//             // when user press Lcontrol
+//             if key == nannou::prelude::Key::LControl {
+//                 model.variables = HashMap::new();
+//                 if let Ok((remaining, ast)) = parser::parser(&model.text_edit) {
+//                     let mut syntactic_analysis = true;
+//                     if remaining != "" {
+//                         syntactic_analysis = false;
+//                     }
+//                     // storing variables
+//                     for x in ast.to_owned() {
+//                         match x {
+//                             Command::DeclareVariable((key, value)) => {
+//                                 // eval Vec<Operation>
+//                                 model.variables.insert(key, 0.0);
+//                             }
+//                             _ => (),
+//                         }
+//                     }
+//                     // we update our model only if
+//                     if syntactic_analysis {
+//                         model.instructions = ast;
+//                     } else {
+//                         println!("{}", "CAN'T UPDATE ABSTRACT SYNTAX TREE".red().bold());
+//                         println!("{:#?}", parser::parser(&model.text_edit));
+//                         println!("{} {}", "error:".red().bold(), &remaining.red().bold());
+//                     }
+//                 }
+//             }
+//         }
+//         KeyReleased(_key) => {}
+//         MouseMoved(_pos) => {}
+//         MousePressed(_button) => {}
+//         MouseReleased(_button) => {}
+//         MouseEntered => {}
+//         MouseExited => {}
+//         MouseWheel(_amount, _phase) => {}
+//         Moved(_pos) => {}
+//         Resized(_size) => {}
+//         Touch(_touch) => {}
+//         TouchPressure(_pressure) => {}
+//         HoveredFile(_path) => {}
+//         DroppedFile(_path) => {}
+//         HoveredFileCancelled => {}
+//         Focused => {}
+//         Unfocused => {}
+//         Closed => {}
+//     }
+// }
 
-fn event(_app: &App, _model: &mut Model, event: Event) {
-    match event {
-        Event::WindowEvent {
-            id: _,
-            raw: _,
-            simple: _,
-        } => {}
-        Event::DeviceEvent(_device_id, _event) => {}
-        Event::Update(_dt) => {}
-        Event::Awakened => {}
-        Event::Suspended(_b) => {}
-    }
-}
+// fn event(_app: &App, _model: &mut Model, event: Event) {
+//     match event {
+//         Event::WindowEvent {
+//             id: _,
+//             raw: _,
+//             simple: _,
+//         } => {}
+//         Event::DeviceEvent(_device_id, _event) => {}
+//         Event::Update(_dt) => {}
+//         Event::Awakened => {}
+//         Event::Suspended(_b) => {}
+//     }
+// }
 
-// all the events must be implemented!
+// // all the events must be implemented!
 
-fn raw_window_event(_app: &App, _model: &mut Model, _event: nannou::winit::WindowEvent) {}
+// fn raw_window_event(_app: &App, _model: &mut Model, _event: nannou::winit::WindowEvent) {}
 
-fn key_pressed(_app: &App, _model: &mut Model, _key: Key) {}
+// fn key_pressed(_app: &App, _model: &mut Model, _key: Key) {}
 
-fn key_released(_app: &App, _model: &mut Model, _key: Key) {}
+// fn key_released(_app: &App, _model: &mut Model, _key: Key) {}
 
-fn mouse_moved(_app: &App, _model: &mut Model, _pos: Point2) {}
+// fn mouse_moved(_app: &App, _model: &mut Model, _pos: Point2) {}
 
-fn mouse_pressed(_app: &App, _model: &mut Model, _button: MouseButton) {}
+// fn mouse_pressed(_app: &App, _model: &mut Model, _button: MouseButton) {}
 
-fn mouse_released(_app: &App, _model: &mut Model, _button: MouseButton) {}
+// fn mouse_released(_app: &App, _model: &mut Model, _button: MouseButton) {}
 
-fn mouse_wheel(_app: &App, _model: &mut Model, _dt: MouseScrollDelta, _phase: TouchPhase) {}
+// fn mouse_wheel(_app: &App, _model: &mut Model, _dt: MouseScrollDelta, _phase: TouchPhase) {}
 
-fn mouse_entered(_app: &App, _model: &mut Model) {}
+// fn mouse_entered(_app: &App, _model: &mut Model) {}
 
-fn mouse_exited(_app: &App, _model: &mut Model) {}
+// fn mouse_exited(_app: &App, _model: &mut Model) {}
 
-fn window_moved(_app: &App, _model: &mut Model, _pos: Point2) {}
+// fn window_moved(_app: &App, _model: &mut Model, _pos: Point2) {}
 
-fn window_resized(_app: &App, _model: &mut Model, _dim: Vector2) {}
+// fn window_resized(_app: &App, _model: &mut Model, _dim: Vector2) {}
 
-fn window_focused(_app: &App, _model: &mut Model) {}
+// fn window_focused(_app: &App, _model: &mut Model) {}
 
-fn window_unfocused(_app: &App, _model: &mut Model) {}
+// fn window_unfocused(_app: &App, _model: &mut Model) {}
 
-fn window_closed(_app: &App, _model: &mut Model) {}
+// fn window_closed(_app: &App, _model: &mut Model) {}
--- src/parser.rs
+++ src/parser.rs
@@ -4,237 +4,186 @@
 // **IMPORTANT** MVC has just to do DrawShapeWf32(shape, val1, val2) where val1 and val2 are f32!
 // i need an HashMap inside the parser because we need to keep track of all variables in the context.
 use nom::branch::alt;
-use nom::bytes::complete::tag;
-use nom::character::complete::{alpha1, char, multispace0, space0};
+use nom::character::complete::{alpha1, char, multispace0, one_of, space0};
 use nom::combinator::map;
 use nom::error::VerboseError as Error;
-use nom::multi::many0;
+use nom::multi::{fold_many0, many0};
 use nom::number::complete::float;
-use nom::sequence::{delimited, pair, preceded, terminated, tuple};
+use nom::sequence::{pair, preceded, terminated, tuple};
 use nom::IResult;
-use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Command {
-    VariableName(String),
-    // x: expr
-    DeclareVariable((String, Vec<Operation>)),
-    // box | circle
-    DrawShape(String),
-    // box var var
-    DrawShapeWf32((String, Vec<Operation>, Vec<Operation>)),
+pub enum Builtin {
+    Plus,
+    Minus,
+    Mult,
+    Div,
 }
 
 #[derive(Debug, PartialEq, Clone)]
-enum Value {
+pub enum Factor {
     Variable(String),
     Number(f32),
-    Operation(Box<Operation>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Atom {
+    Factor(Factor),
+    Builtin(Builtin),
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Operation {
-    Identity(Value),
-    Plus((Value, Value)),
-    Minus((Value, Value)),
-    Mult((Value, Value)),
-    Div((Value, Value)),
+    Identity(Factor),
+    Calculation((Box<Operation>, Builtin, Box<Operation>)),
 }
 
-// it recognizes a variable name like "x", "y", "xy", "myVariablE"
-fn variable_name(input: &str) -> IResult<&str, String, Error<&str>> {
+#[derive(Debug, PartialEq, Clone)]
+pub enum Expression {
+    Constant(Atom),
+    Declaration((String, Operation)),
+}
+
+pub fn variable_name(input: &str) -> IResult<&str, String, Error<&str>> {
     map(alpha1, |x: &str| x.to_string())(input)
 }
-// it recognizes pattern **x: expr**
-fn assignment(input: &str) -> IResult<&str, (String, Vec<Operation>), Error<&str>> {
+
+pub fn assignment(input: &str) -> IResult<&str, (String, Operation), Error<&str>> {
     map(
         tuple((variable_name, space0, char(':'), space0, expr)),
         |(variable_name, _, _, _, value)| (variable_name, value),
     )(input)
 }
 
-fn value(input: &str) -> IResult<&str, Value, Error<&str>> {
-    alt((
-        map(alpha1, |variable: &str| {
-            Value::Variable(variable.to_string())
-        }),
-        map(float, |number: f32| Value::Number(number)),
-    ))(input)
-}
-
-// it recognizes pattern **box**
-fn declare_box(input: &str) -> IResult<&str, Command, Error<&str>> {
-    map(tag("box"), |shape: &str| {
-        Command::DrawShape(shape.to_string())
+pub fn builtin(input: &str) -> IResult<&str, Atom, Error<&str>> {
+    map(one_of("+-*/"), |op| match op {
+        '+' => Atom::Builtin(Builtin::Plus),
+        '-' => Atom::Builtin(Builtin::Minus),
+        '*' => Atom::Builtin(Builtin::Mult),
+        '/' => Atom::Builtin(Builtin::Div),
+        _ => unreachable!(),
     })(input)
 }
 
-// *****box variable*****
-// So here if we find ******box variable***** we have to find the value of the variable on the HashMap and set the command
-// Command::DrawShapeWf32((shape.to_string(), val1, val2))
-fn declare_cmp_box(input: &str) -> IResult<&str, Command, Error<&str>> {
-    alt((
-        map(
-            tuple((tag("box"), space0, expr, space0, expr)),
-            |(shape, _, val1, _, val2): (&str, _, _, _, _)| {
-                Command::DrawShapeWf32((shape.to_string(), val1, val2))
-            },
-        ),
-        map(
-            tuple((tag("box"), space0, expr)),
-            |(x, _, value): (&str, _, Vec<Operation>)| {
-                Command::DrawShapeWf32((x.to_string(), value.clone(), value.clone()))
-            },
-        ),
-    ))(input)
+pub fn factor(input: &str) -> IResult<&str, Factor, Error<&str>> {
+    map(
+        tuple((
+            space0,
+            alt((
+                map(alpha1, |variable: &str| {
+                    Factor::Variable(variable.to_string())
+                }),
+                map(float, |number: f32| Factor::Number(number)),
+            )),
+            space0,
+        )),
+        |(_, fac, _)| fac,
+    )(input)
 }
 
-// We parse any expr surrounded by parens, ignoring all whitespaces around those
-// fn parens(i: &str) -> IResult<&str, f32, Error<&str>> {
-//     delimited(space0, delimited(tag("("), expr, tag(")")), space0)(i)
-// }
-
-fn factor(input: &str) -> IResult<&str, Value, Error<&str>> {
-    map(tuple((space0, value, space0)), |(_, value, _)| value)(input)
+pub fn atom(input: &str) -> IResult<&str, Atom, Error<&str>> {
+    alt((map(factor, |f| Atom::Factor(f)), builtin))(input)
 }
 
-fn term(input: &str) -> IResult<&str, Vec<Operation>, Error<&str>> {
-    let (first_input, first) = factor(input)?;
-    let (input, more) = many0(map(
-        tuple((factor, alt((char('*'), char('/'))), factor)),
-        |(first, op, second)| {
-            if op == '*' {
-                Operation::Mult((first, second))
-            } else {
-                Operation::Div((first, second))
-            }
+pub fn expr(input: &str) -> IResult<&str, Operation, Error<&str>> {
+    let (first_input, first_factor) = factor(input)?;
+    let first_operation = Operation::Identity(first_factor);
+    let (input, operation) = fold_many0(
+        pair(builtin, factor),
+        first_operation.clone(),
+        |acc, (op, next_factor)| match op {
+            Atom::Builtin(operator) => Operation::Calculation((
+                Box::new(acc),
+                operator,
+                Box::new(Operation::Identity(next_factor)),
+            )),
+            _ => unreachable!(),
         },
-    ))(input)?;
-    // what
-    if more.len() == 0 {
-        Ok((first_input, vec![Operation::Identity(first)]))
+    )(first_input)?;
+    if first_input == "" {
+        Ok((first_input, first_operation))
     } else {
-        Ok((input, more))
+        Ok((input, operation))
     }
 }
 
-pub fn expr(input: &str) -> IResult<&str, Vec<Operation>, Error<&str>> {
-    let (input, mut operations) = term(input)?;
-    let (input, mut more) = many0(map(
-        tuple((factor, alt((char('+'), char('-'))), factor)),
-        |(first, op, second)| {
-            if op == '+' {
-                Operation::Plus((first, second))
-            } else {
-                Operation::Minus((first, second))
-            }
-        },
-    ))(input)?;
-    let mut res = vec![];
-    res.append(&mut operations);
-    res.append(&mut more);
-    Ok((input, res))
-}
-
-pub fn parser(input: &str) -> IResult<&str, Vec<Command>, Error<&str>> {
+pub fn parser(input: &str) -> IResult<&str, Vec<Expression>, Error<&str>> {
     many0(terminated(
-        alt((
-            preceded(multispace0, declare_cmp_box),
-            // inside alt combinator all the functions have to be of the same type.. we choose Command type
-            preceded(
-                multispace0,
-                map(assignment, |(name, val): (String, Vec<Operation>)| {
-                    Command::DeclareVariable((name, val))
-                }),
-            ),
-            preceded(multispace0, declare_box),
-        )),
+        preceded(
+            multispace0,
+            map(assignment, |(name, operation): (String, Operation)| {
+                Expression::Declaration((name, operation))
+            }),
+        ),
         multispace0,
     ))(input)
-}
-
-fn eval(input: Vec<Operation>) -> f32 {
-    let mut evaluated = 0.0;
-    for x in input {
-        match x {
-            Operation::Identity(val) => match val {
-                Value::Number(x) => {
-                    evaluated = x;
-                }
-                Value::Variable(_) => {}
-                Value::Operation(_) => {}
-            },
-            Operation::Plus((_, _)) => {}
-            Operation::Minus((_, _)) => {}
-            Operation::Mult((_, _)) => {}
-            Operation::Div((_, _)) => {}
-        }
-    }
-    evaluated
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashMap;
 
-    #[test]
-    fn declare_variable() {
-        let mut variables: HashMap<String, f32> = HashMap::new();
-        let (_, commands) = parser("x: 2").unwrap();
-        for command in commands {
-            if let Command::DeclareVariable((name, value)) = command {
-                variables.insert(name, eval(value));
-            }
-        }
-        assert_eq!(variables.get("x"), Some(&2.0));
-    }
-    #[test]
-    fn test_simple_expression() {
-        let expression = "x: 3\ny: 1\nz: y * 2.0\n";
-        let (rest, ast) = parser(expression).unwrap();
-        assert_eq!(
-            ast[0],
-            Command::DeclareVariable((
-                "x".to_string(),
-                vec![Operation::Identity(Value::Number(3.0))]
-            ))
-        );
-        assert_eq!(
-            ast[1],
-            Command::DeclareVariable((
-                "y".to_string(),
-                vec![Operation::Identity(Value::Number(1.0))]
-            ))
-        );
-        assert_eq!(
-            ast[2],
-            Command::DeclareVariable((
-                "z".to_string(),
-                vec![Operation::Mult((
-                    Value::Variable("y".to_string()),
-                    Value::Number(2.0)
-                ))]
-            ))
-        );
-        assert_eq!(rest, "");
-    }
+    // #[test]
+    // fn declare_variable() {
+    //     let mut variables: HashMap<String, f32> = HashMap::new();
+    //     let (_, commands) = parser("x: 2").unwrap();
+    //     for command in commands {
+    //         if let Command::DeclareVariable((name, value)) = command {
+    //             variables.insert(name, eval(value));
+    //         }
+    //     }
+    //     assert_eq!(variables.get("x"), Some(&2.0));
+    // }
+    // #[test]
+    // fn test_simple_expression() {
+    //     let expression = "x: 3\ny: 1\nz: y * 2.0\n";
+    //     let (rest, ast) = parser(expression).unwrap();
+    //     assert_eq!(
+    //         ast[0],
+    //         Command::DeclareVariable((
+    //             "x".to_string(),
+    //             vec![Operation::Identity(Value::Number(3.0))]
+    //         ))
+    //     );
+    //     assert_eq!(
+    //         ast[1],
+    //         Command::DeclareVariable((
+    //             "y".to_string(),
+    //             vec![Operation::Identity(Value::Number(1.0))]
+    //         ))
+    //     );
+    //     assert_eq!(
+    //         ast[2],
+    //         Command::DeclareVariable((
+    //             "z".to_string(),
+    //             vec![Operation::Mult((
+    //                 Value::Variable("y".to_string()),
+    //                 Value::Number(2.0)
+    //             ))]
+    //         ))
+    //     );
+    //     assert_eq!(rest, "");
+    // }
 
     #[test]
     fn test_several_operations() {
-        let expression = "x: 3\ny: 1\nz: y + 2.0 * x\n";
+        let expression = "z: y + 2.0 * x\n";
         let (rest, ast) = parser(expression).unwrap();
-        assert_eq!(
-            ast[2],
-            Command::DeclareVariable((
-                "z".to_string(),
-                vec![Operation::Plus((
-                    Value::Variable("y".to_string()),
-                    Value::Operation(Box::new(Operation::Mult((
-                        Value::Number(2.0),
-                        Value::Variable("x".to_string())
-                    ))))
-                ))]
-            ))
-        );
+        dbg!(ast.clone());
+        assert_eq!(false, true);
+        // assert_eq!(
+        //     ast[0],
+        //     Command::DeclareVariable((
+        //         "z".to_string(),
+        //         vec![Operation::Plus((
+        //             Value::Variable("y".to_string()),
+        //             Value::Operation(Box::new(Operation::Mult((
+        //                 Value::Number(2.0),
+        //                 Value::Variable("x".to_string())
+        //             ))))
+        //         ))]
+        //     ))
+        // );
     }
 }
