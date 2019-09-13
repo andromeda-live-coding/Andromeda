d711d34a883e63a262c25313fccfea6077f77ccd
thealchemist17 <lucamancinelli17@gmail.com>
2019-09-13 12:50:16 +0200

    working on the evaluation of expressions

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn get_value(factor: Factor, variables: &HashMap<String, f32>) -> f32 {
     }
 }
 
+// need something here
 fn eval_calculation(expr: Box<Operation>, variables: &HashMap<String, f32>) -> f32 {
     match *expr {
         Operation::Identity(value) => get_value(value, variables),
@@ -21,28 +22,22 @@ fn declare_variable(
     (name, value): (String, Operation),
     variables: &HashMap<String, f32>,
 ) -> (String, f32) {
+    //dbg!(value.clone());
     match value {
         Operation::Identity(factor) => (name, get_value(factor, variables)),
         Operation::Calculation((first, op, second)) => {
             let first = match *first {
                 Operation::Identity(first) => get_value(first, variables),
                 // TODO: This should be implemented // MAYBE NOT TO EVALUATE EXPRESSIONS (IT DO OTHER THINGS)
-                Operation::Calculation((_first2, _op2, _second2)) => unimplemented!(),
+                Operation::Calculation((_first2, _op2, _second2)) => unimplemented!(), // OK
             };
+            ///////////////////////////////////////////////////////////////////////////////////////
             let second = match *second {
-                Operation::Identity(second) => get_value(second, variables),
+                Operation::Identity(second) => get_value(second, variables), // OK
                 // TODO: This should be implemented
-                Operation::Calculation((_first2, _op2, second2)) => {
-                    match *second2 {
-                        Operation::Identity(x) => get_value(x, variables),
-                        // if second2 is not an identity, it means it is a Calculation where the third element of the tuple can be a Calculation..
-                        // so we have to loop until the third element is a Identity!
-                        Operation::Calculation((_first_r, _op_r, s_recursive)) => {
-                            eval_calculation(s_recursive, variables)
-                        }
-                    }
-                }
+                Operation::Calculation((first2, _op2, second2)) => unimplemented!(),
             };
+            ///////////////////////////////////////////////////////////////////////////////////////
             match op {
                 Builtin::Plus => (name, first + second),
                 Builtin::Minus => (name, first - second),
@@ -54,11 +49,12 @@ fn declare_variable(
 }
 
 fn main() {
-    let content = "x: 2\ny: x\nz: x + 2";
+    //let content = "x: 2\ny: x\nz: x + 2";
     // TODO: Try with this content
     let content2 = "x: 2\ny: x\nz: x + 2 + 3";
     //let content3 = "x: 2\ny: x\nz: x + y + 1 +1 ";
     let (_, ast) = parser(content2).unwrap();
+    dbg!(ast.clone());
     let mut variables: HashMap<String, f32> = HashMap::new();
     for expression in ast {
         match expression {
@@ -68,7 +64,5 @@ fn main() {
             }
         }
     }
-    let (_, ast2) = parser(content2).unwrap();
-    dbg!(ast2);
     dbg!(variables.clone());
 }
