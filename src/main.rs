151e1d3352d28b18365116d1e150ed34393c749c
thealchemist17 <lucamancinelli17@gmail.com>
2019-09-13 11:33:41 +0200

    close to parsing cmp expressions

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,13 @@ fn get_value(factor: Factor, variables: &HashMap<String, f32>) -> f32 {
     }
 }
 
+fn eval_calculation(expr: Box<Operation>, variables: &HashMap<String, f32>) -> f32 {
+    match *expr {
+        Operation::Identity(value) => get_value(value, variables),
+        Operation::Calculation((first, op, second)) => eval_calculation(second, variables),
+    }
+}
+
 fn declare_variable(
     (name, value): (String, Operation),
     variables: &HashMap<String, f32>,
@@ -19,13 +26,22 @@ fn declare_variable(
         Operation::Calculation((first, op, second)) => {
             let first = match *first {
                 Operation::Identity(first) => get_value(first, variables),
-                // TODO: This should be implemented
-                _ => unimplemented!(),
+                // TODO: This should be implemented // MAYBE NOT TO EVALUATE EXPRESSIONS (IT DO OTHER THINGS)
+                Operation::Calculation((_first2, _op2, _second2)) => unimplemented!(),
             };
             let second = match *second {
                 Operation::Identity(second) => get_value(second, variables),
                 // TODO: This should be implemented
-                _ => unimplemented!(),
+                Operation::Calculation((_first2, _op2, second2)) => {
+                    match *second2 {
+                        Operation::Identity(x) => get_value(x, variables),
+                        // if second2 is not an identity, it means it is a Calculation where the third element of the tuple can be a Calculation..
+                        // so we have to loop until the third element is a Identity!
+                        Operation::Calculation((_first_r, _op_r, s_recursive)) => {
+                            eval_calculation(s_recursive, variables)
+                        }
+                    }
+                }
             };
             match op {
                 Builtin::Plus => (name, first + second),
@@ -40,8 +56,9 @@ fn declare_variable(
 fn main() {
     let content = "x: 2\ny: x\nz: x + 2";
     // TODO: Try with this content
-    // let content = "x: 2\ny: x\nz: x + 2 + 3";
-    let (_, ast) = parser(content).unwrap();
+    let content2 = "x: 2\ny: x\nz: x + 2 + 3";
+    //let content3 = "x: 2\ny: x\nz: x + y + 1 +1 ";
+    let (_, ast) = parser(content2).unwrap();
     let mut variables: HashMap<String, f32> = HashMap::new();
     for expression in ast {
         match expression {
@@ -51,5 +68,7 @@ fn main() {
             }
         }
     }
+    let (_, ast2) = parser(content2).unwrap();
+    dbg!(ast2);
     dbg!(variables.clone());
 }
